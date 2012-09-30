b87ccfe2e81a3e53524efd4e786eb8ea394499d7
Stephen Neyens <neyens.s@gmail.com>
2012-09-30 17:38:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountMeIn::Application.config.secret_token = 'd96721f2cc84f15de4993e44ee0dfe0c496d1bf5213889cab5f3e02484e149b1868c2273380d19d57a56f87a9051e0fbc386f55c3e4639d8d266d876eea5e1f7'
