22d6ac3c60a9734bfd12b2c21e49f0aded8b792e
Stephen Neyens <neyens.s@gmail.com>
2012-10-10 22:56:12 -0400

    Added session[:id] = 1 to test to remove error in functional tests.

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -3,21 +3,25 @@ require 'test_helper'
 class SessionsControllerTest < ActionController::TestCase
   test "should get login," do
     get :login,
+    session[:id] = 1
     assert_response :success
   end
 
   test "should get home," do
     get :home,
+    session[:id] = 1
     assert_response :success
   end
 
   test "should get profile," do
     get :profile,
+    session[:id] = 1
     assert_response :success
   end
 
   test "should get setting" do
     get :setting
+    session[:id] = 1
     assert_response :success
   end
 
