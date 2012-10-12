b49161792cc21258a22d66d5b5464df8aaa0ef18
Stephen Neyens <neyens.s@gmail.com>
2012-10-12 01:01:33 -0400

    Updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Will write stories and focus on basic framework soon.
+Added my own implementation of friendships.
+
+Ensures that user numericality where left_col < right_col is present. Uniqueness checks as well. If current user has ID > friend ID, then temp swap and save to database.
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,26 @@
 class FriendshipsController < ApplicationController
+  
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    
+    #friend_id = 1
+    friend_id = params[:friend_id].to_i
+    
+    # if 2 > 1
+    if current_user.id > friend_id
+      # friend_id = 2
+      friend_id = current_user.id
+      
+      # new_curr = 1
+      new_current_user_id = current_user.id
+      
+      user = User.find(params[:friend_id])
+    else
+      user = current_user
+    end
+    
+    #friend should build relationship with requestee if id is less
+    @friendship = user.friendships.build(:friend_id => friend_id, :initiator => current_user.id)
+    
     if @friendship.save
       flash[:notice] = "Added friend."
       redirect_to root_url
