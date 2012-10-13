4bab769361835bdbfb7612b5c583005bab57efec
Stephen Neyens <neyens.s@gmail.com>
2012-10-13 12:53:21 -0400

    Modified select statement for searching users to remove hash and salt from being selected. (Huge security risk).

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121009143606) do
+ActiveRecord::Schema.define(:version => 20121012202009) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "location"
+    t.integer  "user_id"
+    t.boolean  "public"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "friendships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.integer  "initiator"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
