bd50ccc9e3c9fe6eb7f51502225d9841be18a361
Stephen Neyens <neyens.s@gmail.com>
2012-10-14 11:29:01 -0400

    Can now use current_user or current_user.id to get the session user's id.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,12 @@ class ApplicationController < ActionController::Base
         return true
       end
     end
+    
+    private
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    helper_method :current_user
 end
